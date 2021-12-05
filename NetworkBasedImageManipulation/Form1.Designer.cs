72b69c32720f5b6bfd24bc1321753d13e79076f5
v-k-k <vladimirkrishtop@gmail.com>
2021-12-05 21:51:25 +0200

    Initial commit

---
+++ NetworkBasedImageManipulation/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace NetworkBasedImageManipulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxImageContainer = new System.Windows.Forms.PictureBox();
+            this.buttonLoadImage = new System.Windows.Forms.Button();
+            this.textBoxImagePath = new System.Windows.Forms.TextBox();
+            this.openImageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.checkBoxMakeGray = new System.Windows.Forms.CheckBox();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.trackBarBrightness = new System.Windows.Forms.TrackBar();
+            this.labelBrightness = new System.Windows.Forms.Label();
+            this.labelContrast = new System.Windows.Forms.Label();
+            this.trackBarContrast = new System.Windows.Forms.TrackBar();
+            this.comboBoxGradationCorrection = new System.Windows.Forms.ComboBox();
+            this.labelGradationCorrection = new System.Windows.Forms.Label();
+            this.labelGammaCorrection = new System.Windows.Forms.Label();
+            this.trackBarGammaCorrection = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImageContainer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarContrast)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGammaCorrection)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxImageContainer
+            // 
+            this.pictureBoxImageContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxImageContainer.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxImageContainer.Name = "pictureBoxImageContainer";
+            this.pictureBoxImageContainer.Size = new System.Drawing.Size(586, 470);
+            this.pictureBoxImageContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxImageContainer.TabIndex = 0;
+            this.pictureBoxImageContainer.TabStop = false;
+            // 
+            // buttonLoadImage
+            // 
+            this.buttonLoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLoadImage.Location = new System.Drawing.Point(616, 22);
+            this.buttonLoadImage.Name = "buttonLoadImage";
+            this.buttonLoadImage.Size = new System.Drawing.Size(228, 34);
+            this.buttonLoadImage.TabIndex = 1;
+            this.buttonLoadImage.Text = "Load image";
+            this.buttonLoadImage.UseVisualStyleBackColor = true;
+            this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+            // 
+            // textBoxImagePath
+            // 
+            this.textBoxImagePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxImagePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxImagePath.Location = new System.Drawing.Point(616, 73);
+            this.textBoxImagePath.Name = "textBoxImagePath";
+            this.textBoxImagePath.Size = new System.Drawing.Size(227, 20);
+            this.textBoxImagePath.TabIndex = 2;
+            // 
+            // openImageFileDialog
+            // 
+            this.openImageFileDialog.FileName = "openImageFileDialog";
+            // 
+            // checkBoxMakeGray
+            // 
+            this.checkBoxMakeGray.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxMakeGray.AutoSize = true;
+            this.checkBoxMakeGray.Location = new System.Drawing.Point(621, 400);
+            this.checkBoxMakeGray.Name = "checkBoxMakeGray";
+            this.checkBoxMakeGray.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxMakeGray.TabIndex = 3;
+            this.checkBoxMakeGray.Text = "Make grey";
+            this.checkBoxMakeGray.UseVisualStyleBackColor = true;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApply.Location = new System.Drawing.Point(616, 459);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(230, 23);
+            this.buttonApply.TabIndex = 4;
+            this.buttonApply.Text = "Apply settings";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // trackBarBrightness
+            // 
+            this.trackBarBrightness.LargeChange = 10;
+            this.trackBarBrightness.Location = new System.Drawing.Point(617, 123);
+            this.trackBarBrightness.Maximum = 50;
+            this.trackBarBrightness.Minimum = -50;
+            this.trackBarBrightness.Name = "trackBarBrightness";
+            this.trackBarBrightness.Size = new System.Drawing.Size(225, 45);
+            this.trackBarBrightness.TabIndex = 5;
+            this.trackBarBrightness.Scroll += new System.EventHandler(this.trackBarBrightness_Scroll);
+            // 
+            // labelBrightness
+            // 
+            this.labelBrightness.AutoSize = true;
+            this.labelBrightness.Location = new System.Drawing.Point(699, 107);
+            this.labelBrightness.Name = "labelBrightness";
+            this.labelBrightness.Size = new System.Drawing.Size(56, 13);
+            this.labelBrightness.TabIndex = 6;
+            this.labelBrightness.Text = "Brightness";
+            // 
+            // labelContrast
+            // 
+            this.labelContrast.AutoSize = true;
+            this.labelContrast.Location = new System.Drawing.Point(699, 171);
+            this.labelContrast.Name = "labelContrast";
+            this.labelContrast.Size = new System.Drawing.Size(46, 13);
+            this.labelContrast.TabIndex = 8;
+            this.labelContrast.Text = "Contrast";
+            // 
+            // trackBarContrast
+            // 
+            this.trackBarContrast.LargeChange = 10;
+            this.trackBarContrast.Location = new System.Drawing.Point(617, 187);
+            this.trackBarContrast.Maximum = 50;
+            this.trackBarContrast.Minimum = -50;
+            this.trackBarContrast.Name = "trackBarContrast";
+            this.trackBarContrast.Size = new System.Drawing.Size(225, 45);
+            this.trackBarContrast.TabIndex = 7;
+            this.trackBarContrast.Scroll += new System.EventHandler(this.trackBarContrast_Scroll);
+            // 
+            // comboBoxGradationCorrection
+            // 
+            this.comboBoxGradationCorrection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGradationCorrection.FormattingEnabled = true;
+            this.comboBoxGradationCorrection.Items.AddRange(new object[] {
+            "Linear",
+            "Sinusoid",
+            "Exponential",
+            "Logarithmic"});
+            this.comboBoxGradationCorrection.Location = new System.Drawing.Point(619, 346);
+            this.comboBoxGradationCorrection.Name = "comboBoxGradationCorrection";
+            this.comboBoxGradationCorrection.Size = new System.Drawing.Size(224, 21);
+            this.comboBoxGradationCorrection.TabIndex = 9;
+            // 
+            // labelGradationCorrection
+            // 
+            this.labelGradationCorrection.AutoSize = true;
+            this.labelGradationCorrection.Location = new System.Drawing.Point(676, 327);
+            this.labelGradationCorrection.Name = "labelGradationCorrection";
+            this.labelGradationCorrection.Size = new System.Drawing.Size(103, 13);
+            this.labelGradationCorrection.TabIndex = 10;
+            this.labelGradationCorrection.Text = "Gradation correction";
+            // 
+            // labelGammaCorrection
+            // 
+            this.labelGammaCorrection.AutoSize = true;
+            this.labelGammaCorrection.Location = new System.Drawing.Point(676, 233);
+            this.labelGammaCorrection.Name = "labelGammaCorrection";
+            this.labelGammaCorrection.Size = new System.Drawing.Size(93, 13);
+            this.labelGammaCorrection.TabIndex = 12;
+            this.labelGammaCorrection.Text = "Gamma correction";
+            // 
+            // trackBarGammaCorrection
+            // 
+            this.trackBarGammaCorrection.LargeChange = 10;
+            this.trackBarGammaCorrection.Location = new System.Drawing.Point(616, 249);
+            this.trackBarGammaCorrection.Maximum = 190;
+            this.trackBarGammaCorrection.Minimum = 10;
+            this.trackBarGammaCorrection.Name = "trackBarGammaCorrection";
+            this.trackBarGammaCorrection.Size = new System.Drawing.Size(225, 45);
+            this.trackBarGammaCorrection.TabIndex = 11;
+            this.trackBarGammaCorrection.Value = 10;
+            this.trackBarGammaCorrection.Scroll += new System.EventHandler(this.trackBarGammaCorrection_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 494);
+            this.Controls.Add(this.labelGammaCorrection);
+            this.Controls.Add(this.trackBarGammaCorrection);
+            this.Controls.Add(this.labelGradationCorrection);
+            this.Controls.Add(this.comboBoxGradationCorrection);
+            this.Controls.Add(this.labelContrast);
+            this.Controls.Add(this.trackBarContrast);
+            this.Controls.Add(this.labelBrightness);
+            this.Controls.Add(this.trackBarBrightness);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.checkBoxMakeGray);
+            this.Controls.Add(this.textBoxImagePath);
+            this.Controls.Add(this.buttonLoadImage);
+            this.Controls.Add(this.pictureBoxImageContainer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image manipulation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImageContainer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarContrast)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGammaCorrection)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImageContainer;
+        private System.Windows.Forms.Button buttonLoadImage;
+        private System.Windows.Forms.TextBox textBoxImagePath;
+        private System.Windows.Forms.OpenFileDialog openImageFileDialog;
+        private System.Windows.Forms.CheckBox checkBoxMakeGray;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.TrackBar trackBarBrightness;
+        private System.Windows.Forms.Label labelBrightness;
+        private System.Windows.Forms.Label labelContrast;
+        private System.Windows.Forms.TrackBar trackBarContrast;
+        private System.Windows.Forms.ComboBox comboBoxGradationCorrection;
+        private System.Windows.Forms.Label labelGradationCorrection;
+        private System.Windows.Forms.Label labelGammaCorrection;
+        private System.Windows.Forms.TrackBar trackBarGammaCorrection;
+    }
+}
+
---
+++ NetworkBasedImageManipulation/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkBasedImageManipulation
+{
+    public partial class Form1 : Form
+    {
+        private const string defaultSample = "sample.jpg";
+
+        Bitmap bmp;
+        string brightnessLabel;
+        string contrastLabel;
+        string gammaLabel;
+        private readonly double CorrectionKoef = 8 * Math.Log(2) / 255d;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPicture(defaultSample);
+            brightnessLabel = labelBrightness.Text;
+            contrastLabel = labelContrast.Text;
+            UpdateBrightnessLabel();
+            UpdateContrastLabel();
+            UpdateGammaLabel();
+            comboBoxGradationCorrection.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLoadImage_Click(object sender, EventArgs e)
+        {
+            openImageFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            DialogResult result = openImageFileDialog.ShowDialog();
+            LoadPicture(openImageFileDialog.FileName);
+        }
+
+        private void LoadPicture(string fileName)
+        {
+            try
+            {
+                bmp = new Bitmap(Image.FromFile(fileName));
+                textBoxImagePath.Text = fileName;
+                pictureBoxImageContainer.Image = bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBoxImagePath.Text = "";
+            }
+        }
+
+        private void ChangePicture()
+        {
+            Bitmap greyed = new Bitmap(bmp);
+            bool isGrey = checkBoxMakeGray.Checked;
+            int brightness = trackBarBrightness.Value;
+            int contrast = trackBarContrast.Value;
+            int correctionIndex = comboBoxGradationCorrection.SelectedIndex;
+            int gamma = trackBarGammaCorrection.Value;
+
+            for (int y = 0; y < greyed.Height; y++)
+                for (int x = 0; x < greyed.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    if (isGrey)
+                        pixel = GreyoutPixel(pixel);
+                    if (brightness != 0)
+                        pixel = ChangeBrightness(pixel, brightness);
+                    if (contrast != 0)
+                        pixel = ChangeContrast(pixel, contrast);
+                    switch (correctionIndex)
+                    {
+                        case 0: break;
+                        case 1:
+                            pixel = ChangeCorrection(pixel, CorrectionBySin);
+                            break;
+                        case 2:
+                            pixel = ChangeCorrection(pixel, CorrectionByExp);
+                            break;
+                        case 3:
+                            pixel = ChangeCorrection(pixel, CorrectionByLog);
+                            break;
+                    }
+                    if (gamma != 100)
+                        pixel = ChangeGammaCorrection(pixel, gamma);
+
+                    greyed.SetPixel(x, y, pixel);
+                }
+            pictureBoxImageContainer.Image = greyed;
+        }
+
+        private Color ChangeGammaCorrection(Color pixel, int gamma)
+        {
+            double calculatedGamma = gamma / 100d;
+            return Color.FromArgb(
+                ChangeColorGamma(pixel.R, calculatedGamma),
+                ChangeColorGamma(pixel.G, calculatedGamma),
+                ChangeColorGamma(pixel.B, calculatedGamma)
+            );
+        }
+
+        private int ChangeColorGamma(int color, double calculatedGamma)
+        {
+            double result = Math.Pow(color / 255d, calculatedGamma) * 255;
+            if (result > 255) return 255;
+            return Convert.ToInt16(result);
+        }
+
+        delegate int CorrectionFunction(int color);
+
+        int CorrectionBySin(int color)
+        {
+            double result = (255 / 2d) * Math.Sin(Math.PI / 255 * color - Math.PI / 2) + (255 / 2d);
+            return Convert.ToInt16(result);
+        }
+
+        int CorrectionByExp(int color)
+        {
+            double result = Math.Exp(CorrectionKoef * color) - 1;
+            return Convert.ToInt16(result);
+        }
+
+        int CorrectionByLog(int color)
+        {
+            double result = Math.Log(color + 1) / CorrectionKoef;
+            return Convert.ToInt16(result);
+        }
+
+        private Color ChangeCorrection(Color pixel, CorrectionFunction fn)
+        {
+            return Color.FromArgb(fn(pixel.R), fn(pixel.G), fn(pixel.B));
+        }
+
+        private Color ChangeContrast(Color pixel, int value)
+        {
+            float percent = value / 100f;
+            return Color.FromArgb(
+                ChangeColorContrast(pixel.R, percent),
+                ChangeColorContrast(pixel.G, percent),
+                ChangeColorContrast(pixel.B, percent)
+            );
+        }
+
+        private int ChangeColorContrast(int color, float percent)
+        {
+            int result;
+            if (percent < 0)
+                result = Convert.ToInt16(color + percent * (color - 128));
+            else
+                result = Convert.ToInt16(128 + (color - 128) / (1 - percent));
+
+            if (result > 255) return 255;
+            if (result < 0) return 0;
+            return result;
+        }
+
+        private Color ChangeBrightness(Color pixel, int value)
+        {
+            float percent = value / 100f;
+            return Color.FromArgb(
+                ChangeColorBrightness(pixel.R, percent),
+                ChangeColorBrightness(pixel.G, percent),
+                ChangeColorBrightness(pixel.B, percent)
+            );
+        }
+
+        private int ChangeColorBrightness(int color, float percent)
+        {
+            int result = Convert.ToInt16(color + percent * 128);
+            if (result > 255) return 255;
+            if (result < 0) return 0;
+            return result;
+        }
+
+        private Color GreyoutPixel(Color pixel)
+        {
+            int avg = (pixel.R + pixel.G + pixel.B + 1) / 3;
+            return Color.FromArgb(avg, avg, avg);
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            ChangePicture();
+        }
+
+        private void trackBarBrightness_Scroll(object sender, EventArgs e)
+        {
+            UpdateBrightnessLabel();
+        }
+
+        private void UpdateBrightnessLabel()
+        {
+            labelBrightness.Text = $"{brightnessLabel} {trackBarBrightness.Value}%";
+        }
+
+        private void trackBarContrast_Scroll(object sender, EventArgs e)
+        {
+            UpdateContrastLabel();
+        }
+
+        private void UpdateContrastLabel()
+        {
+            labelContrast.Text = $"{contrastLabel} {trackBarContrast.Value}%";
+        }
+
+        private void UpdateGammaLabel()
+        {
+            labelGammaCorrection.Text = $"{gammaLabel} {trackBarGammaCorrection.Value}%";
+        }
+
+        private void trackBarGammaCorrection_Scroll(object sender, EventArgs e)
+        {
+            UpdateGammaLabel();
+        }
+    }
+}
